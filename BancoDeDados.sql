9eece06a7a1792826e8f9a42740862dd21a333d6
Luca Valadão <lucacv10@gmail.com>
2025-04-01 11:15:57 -0300

    Update BancoDeDados.sql

--- BancoDeDados.sql
+++ BancoDeDados.sql
@@ -1,37 +1,30 @@
 CREATE DATABASE ans_dados;
 
-
-
--- Criar tabelas
 CREATE TABLE IF NOT EXISTS demonstracoes_contabeis (
     id SERIAL PRIMARY KEY,                   
-    data_registro DATE,                      -- Data da informação contábil
-    reg_ans VARCHAR(20),                     -- Registro da operadora na ANS (chave estrangeira)
-    cd_conta_contabil VARCHAR(50),           -- Código da conta contábil
-    descricao VARCHAR(255),                  -- Nome da conta contábil (ex: despesas assistenciais)
-    vl_saldo_final VARCHAR(50),            -- Saldo final (valor da conta no final do período)
-    FOREIGN KEY (reg_ans) REFERENCES operadoras(registro_operadora) -- Relacionamento entre reg_ans e registro_operadora
+    data_registro DATE,                    
+    reg_ans VARCHAR(20),                     
+    cd_conta_contabil VARCHAR(50),           
+    descricao VARCHAR(255),                
+    vl_saldo_final VARCHAR(50),            
+    FOREIGN KEY (reg_ans) REFERENCES operadoras(registro_operadora) 
 );
 
 CREATE TABLE IF NOT EXISTS operadoras (
-    registro_operadora VARCHAR(6) PRIMARY KEY,  -- Identificador único da operadora
-    cnpj VARCHAR(14) UNIQUE,                    -- CNPJ da operadora
-    razao_social VARCHAR(140),                   -- Nome empresarial
-    nome_fantasia VARCHAR(140),                  -- Nome de mercado
-    modalidade VARCHAR(2),                     -- Tipo de operadora (Ex: Medicina de Grupo, Cooperativa, etc.)
-    cidade VARCHAR(30),                         -- Cidade da operadora
-    uf CHAR(2),                                  -- Estado (UF)
-    telefone VARCHAR(20),                        -- Contato
-    endereco_eletronico VARCHAR(255),            -- E-mail
-    representante VARCHAR(50),                  -- Nome do representante legal
-    cargo_representante VARCHAR(40),            -- Cargo do representante
-    data_registro_ANS DATE(8)                       -- Data de registro na ANS
+    registro_operadora VARCHAR(6) PRIMARY KEY,  
+    cnpj VARCHAR(14) UNIQUE,                    
+    razao_social VARCHAR(140),                   
+    nome_fantasia VARCHAR(140),                  
+    modalidade VARCHAR(2),                     
+    cidade VARCHAR(30),                         
+    uf CHAR(2),                                  
+    telefone VARCHAR(20),                        
+    endereco_eletronico VARCHAR(255),            
+    representante VARCHAR(50),                  
+    cargo_representante VARCHAR(40),            
+    data_registro_ANS DATE(8)                       
 );
 
-
-
--- Arquivos
--- Extrai os arquivos pra uma nova pasta fora do formato Zip e RAR
 
 COPY operadoras FROM 'C:\Users\Usuário\OneDrive\Documentos\ArquivosNv\Relatorio_cadop.csv' DELIMITER ';' CSV HEADER; -- Operadoras
 
